d9ca3a9404cb56fb8eafd6d9533c80821bb5d549
JheelPrashar <jheelprashar@gmail.com>
2026-06-04 16:09:25 +0530

    day 2 complete

--- backend/stm/src/main/java/com/example/stm/controller/StudentController.java
+++ backend/stm/src/main/java/com/example/stm/controller/StudentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/students")
 @CrossOrigin(origins="http://localhost:5173")
 public class StudentController {
+    
     @GetMapping
     public Student getStudent(){
         return new Student(1, "jheel", "jheelprashar@gmail.com");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
-function App(){
-  return(
-    <div>
-      <h1>hello</h1>
-    </div>
-  )
+import { useState } from "react";
 
+function App() {
+
+  const [student, setStudent] = useState(null);
+
+  const getStudent = async () => {
+
+    const response = await fetch(
+   "http://localhost:8080/students" );
+    const data = await response.json();
+    setStudent(data);
+  };
+
+  return (
+    <div>
+      <button onClick={getStudent}>
+        Get Student
+      </button>
+          <div>
+            <h2>ID : {student?.id}</h2>
+            <h2>Name : {student?.name}</h2>
+          </div>
+    </div>
+  );
 }
+
+export default App;
